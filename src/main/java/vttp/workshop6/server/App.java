b47bbe731126d8916f9b07f17b4940d69eeeebf9
ngshux <shuxian_98@hotmail.com>
2022-06-21 09:46:03 +0800

    commit

--- src/main/java/vttp/workshop6/server/App.java
+++ src/main/java/vttp/workshop6/server/App.java
@@ -14,19 +14,24 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Server App Started" );
 
-        int port = Integer.parseInt(args[0]);
+        int port = 3001;
+        if(args.length > 0)
+            port = Integer.parseInt(args[0]);
+        
         String cookieFile = args[1];
-
+        System.out.printf( "Server App started at %s\n", port );
         ExecutorService threadPool = Executors.newFixedThreadPool(2);
+
         ServerSocket server = new ServerSocket(port);
 
         while(true){
             System.out.println("Waiting for client connection");
-            Socket socket = server.accept();
-            System.out.println("Connected");
-            CookieClientHandler thr = new CookieClientHandler(socket, cookieFile);
+            Socket sock = server.accept();
+            System.out.println("Connected ...");
+            CookieClientHandler thr = new CookieClientHandler(sock, cookieFile);
+            threadPool.submit(thr);
+            System.out.println("Submitted to threadpool");
         }
 
     }
